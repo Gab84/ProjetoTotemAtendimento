557713ae1d85ee5dde4645487d5f5e93b8c21261
Davi-Silv-a <davi-0001@hotmail.com>
2025-04-01 21:29:54 -0300

    teste commit
    teste verificar arquivos

--- ProjetoTotem/Form1.Designer.cs
+++ ProjetoTotem/Form1.Designer.cs
@@ -31,7 +31,14 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // imageList1
@@ -43,24 +50,82 @@
             this.imageList1.Images.SetKeyName(2, "Tem um problema (4).png");
             this.imageList1.Images.SetKeyName(3, "Tem um problema (5).png");
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoTotem.Properties.Resources.img_bv;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(816, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::ProjetoTotem.Properties.Resources.bt_51;
+            this.button5.Location = new System.Drawing.Point(1333, 689);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(364, 116);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::ProjetoTotem.Properties.Resources.bt_41;
+            this.button3.Location = new System.Drawing.Point(602, 778);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(364, 116);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::ProjetoTotem.Properties.Resources.bt_31;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button4.Location = new System.Drawing.Point(148, 778);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(364, 116);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::ProjetoTotem.Properties.Resources.bt_21;
+            this.button2.Location = new System.Drawing.Point(602, 615);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(364, 116);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(83, 228);
+            this.button1.BackgroundImage = global::ProjetoTotem.Properties.Resources.bt_11;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(148, 615);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(375, 122);
+            this.button1.Size = new System.Drawing.Size(364, 116);
             this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +134,12 @@
 
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
